fbd22dd3040b07473eae17406255d112050e9eb9
patrickmcdias <patrickmullerjp@hotmail.com>
2017-08-26 10:19:01 -0300

    Delete monit.php

--- monit.php
+++
@@ -1,9 +0,0 @@
-<?php
-			$response = shell_exec('./monitor.sh');
-			//$test = shell_exec('./info-adpt.sh');
-			//header('Content-Type: apllication/json');
-			header('Access-Control-Allow-Origin: *');
-
-			//$ph = shell_exec('php main.php');
-			echo $response;
-		?>
